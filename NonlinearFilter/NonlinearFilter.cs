f3bb3490769a024cbe5c5a1db5944c5ed6123f75
Никита Скрынник <PM74.Skrynnik@outlook.com>
2020-12-09 22:39:53 +0700

    New Version

--- NonlinearFilter/NonlinearFilter.cs
+++ NonlinearFilter/NonlinearFilter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace NonlinearFilter
 {
@@ -8,11 +7,7 @@ namespace NonlinearFilter
 		public int XCount;
 		public int YCount;
 
-		public Vector InitialState;
-		public Matrix InitialP;
-
 		public double dt;
-		public Vector[] Measurements;
 
 		public Func<Vector, double>[] f;
 		public Func<double, double>[] u;
@@ -28,46 +23,33 @@ namespace NonlinearFilter
 		public FilterInfo Info;
 		public NonlinearFilter(FilterInfo info) => Info = info;
 
-		public List<Vector> X = new List<Vector>();
-		public List<Matrix> P = new List<Matrix>();
-
 		double CurrentTime = 0.0;
 
-		public void Solve(int iterCount)
+		public (Vector, Matrix) GetNext(Vector state, Matrix P, Vector Measurement)
 		{
-			Vector state_k_k = Info.InitialState;
-			Matrix P_k_k = Info.InitialP;
+			CurrentTime += Info.dt;
 
-			X.Add(state_k_k);
-			P.Add(P_k_k);
+			Vector state_k_k = state;
+			Matrix P_k_k = P;
 
-			for (int i = 0; i < iterCount; i++)
-			{
-				CurrentTime = (i + 1) * Info.dt;
+			Matrix F = CalculateF(state_k_k);
 
-				Matrix F = CalculateF(state_k_k);
+			// First step
+			Vector state_k1_k = GetNextState(state_k_k);
+			Matrix P_k1_k = F * P_k_k * F.Transpose() + Info.Q;
 
-				// First step
-				Vector state_k1_k = GetNextState(state_k_k);
-				Matrix P_k1_k = F * P_k_k * F.Transpose() + Info.Q;
+			// Second step
+			Matrix H = CalculateH(state_k1_k);
 
-				// Second step
-				Matrix H = CalculateH(state_k1_k);
+			Vector y_k1 = Measurement;
+			Vector h = Geth(state_k1_k);
 
-				Vector y_k1 = Info.Measurements[i + 1];
-				Vector h = Geth(state_k1_k);
+			Matrix K = P_k1_k * H.Transpose() * (H * P_k1_k * H.Transpose() + Info.R).Reverse();
+			Vector state_k1_k1 = state_k1_k + K * (y_k1 - h);
 
-				Matrix K = P_k1_k * H.Transpose() * (H * P_k1_k * H.Transpose() + Info.R).Reverse();
-				Vector state_k1_k1 = state_k1_k + K * (y_k1 - h);
+			Matrix P_k1_k1 = (Matrix.Identity(K.N) - K * H) * P_k1_k;
 
-				Matrix P_k1_k1 = (Matrix.Identity(K.N) - K * H) * P_k1_k;
-
-				X.Add(state_k1_k1);
-				P.Add(P_k1_k1);
-
-				state_k_k = state_k1_k1;
-				P_k_k = P_k1_k1;
-			}
+			return (state_k1_k1, P_k1_k1);
 		}
 
 		Matrix CalculateF(Vector state)
@@ -113,88 +95,73 @@ namespace NonlinearFilter
 
 	class OneStepIterationFilter
 	{
-		public FilterInfo Info;
+		public FilterInfo Info { get; set; }
 		public OneStepIterationFilter(FilterInfo info) => Info = info;
 
-		public List<Vector> X = new List<Vector>();
-		public List<Matrix> P = new List<Matrix>();
+		double CurrentTime { get; set; } = 0.0;
 
-		double CurrentTime = 0.0;
-
-		public void Solve(int iterCount)
+		public (Vector, Matrix) GetNext(Vector state, Matrix P, Vector Measurement)
 		{
-			Vector state_k_k = Info.InitialState;
-			Matrix P_k_k = Info.InitialP;
+			CurrentTime += Info.dt;
 
-			X.Add(state_k_k);
-			P.Add(P_k_k);
+			Vector state_k_k = state;
+			Matrix P_k_k = P;
 
-			for (int i = 0; i < iterCount; i++)
+			Vector state_k1_k;
+			Matrix P_k1_k;
+			// Step 1
 			{
-				CurrentTime = (i + 1) * Info.dt;
-
-				Vector state_k1_k;
-				Matrix P_k1_k;
-				// Step 1
-				{
-					Matrix F = CalculateF(state_k_k);
-					state_k1_k = GetNextState(state_k_k);
-					P_k1_k = F * P_k_k * F.Transpose() + Info.Q;
-				}
-
-				Vector state_k1_k1;
-				Matrix P_k1_k1;
-				// Step 2
-				{
-					Matrix H = CalculateH(state_k1_k);
-					Vector y_k1 = Info.Measurements[i + 1];
-					Vector h = Geth(state_k1_k);
-					Matrix K = P_k1_k * H.Transpose() * (H * P_k1_k * H.Transpose() + Info.R).Reverse();
-
-					state_k1_k1 = state_k1_k + K * (y_k1 - h);
-					P_k1_k1 = (Matrix.Identity(K.N) - K * H) * P_k1_k;
-				}
-
-				Vector state_k_k1;
-				// Step 3
-				{
-					Matrix H = CalculateH(state_k1_k1);
-					Matrix F = CalculateF(state_k_k);
-					Vector y_k1 = Info.Measurements[i + 1];
-					Matrix K = P_k_k * F.Transpose() * H.Transpose() * (H * P_k1_k * H.Transpose() + Info.R).Reverse();
-					state_k_k1 = state_k_k + K * (y_k1 - H * state_k1_k);
-				}
-
-				Vector new_state_k1_k;
-				Matrix new_P_k1_k;
-				// Step 4
-				{
-					Vector state_p_k = state_k_k1;
-					Vector state_p_k1 = GetNextState(state_p_k);
-					Matrix F = CalculateF(state_p_k);
-					new_state_k1_k = state_p_k1 + F * (state_k_k - state_p_k);
-					new_P_k1_k = F * P_k_k * F.Transpose() + Info.Q;
-				}
-
-				Vector new_state_k1_k1;
-				Matrix new_P_k1_k1;
-				// Step 5
-				{
-					Matrix H = CalculateH(new_state_k1_k);
-					Vector y_k1 = Info.Measurements[i + 1];
-					Vector h = Geth(new_state_k1_k);
-					Matrix K = new_P_k1_k * H.Transpose() * (H * new_P_k1_k * H.Transpose() + Info.R).Reverse();
-
-					new_state_k1_k1 = new_state_k1_k + K * (y_k1 - h);
-					new_P_k1_k1 = (Matrix.Identity(K.N) - K * H) * new_P_k1_k;
-				}
-
-				X.Add(new_state_k1_k1);
-				P.Add(new_P_k1_k1);
-
-				state_k_k = new_state_k1_k1;
-				P_k_k = new_P_k1_k1;
+				Matrix F = CalculateF(state_k_k);
+				state_k1_k = GetNextState(state_k_k, CurrentTime);
+				P_k1_k = F * P_k_k * F.Transpose() + Info.Q;
 			}
+
+			Vector state_k1_k1;
+			// Step 2
+			{
+				Matrix H = CalculateH(state_k1_k);
+				Vector y_k1 = Measurement;
+				Vector h = Geth(state_k1_k);
+				Matrix K = P_k1_k * H.Transpose() * (H * P_k1_k * H.Transpose() + Info.R).Reverse();
+
+				state_k1_k1 = state_k1_k + K * (y_k1 - h);
+			}
+
+			Vector state_k_k1;
+			// Step 3
+			{
+				Matrix H = CalculateH(state_k1_k1);
+				Matrix F = CalculateF(state_k_k);
+				Vector y_k1 = Measurement;
+				Matrix K = P_k_k * F.Transpose() * H.Transpose() * (H * P_k1_k * H.Transpose() + Info.R).Reverse();
+				state_k_k1 = state_k_k + K * (y_k1 - H * state_k1_k);
+			}
+
+			Vector new_state_k1_k;
+			Matrix new_P_k1_k;
+			// Step 4
+			{
+				Vector state_p_k = state_k_k1;
+				Vector state_p_k1 = GetNextState(state_p_k, CurrentTime);
+				Matrix F = CalculateF(state_p_k);
+				new_state_k1_k = state_p_k1 + F * (state_k_k - state_p_k);
+				new_P_k1_k = F * P_k_k * F.Transpose() + Info.Q;
+			}
+
+			Vector new_state_k1_k1;
+			Matrix new_P_k1_k1;
+			// Step 5
+			{
+				Matrix H = CalculateH(new_state_k1_k);
+				Vector y_k1 = Measurement;
+				Vector h = Geth(new_state_k1_k);
+				Matrix K = new_P_k1_k * H.Transpose() * (H * new_P_k1_k * H.Transpose() + Info.R).Reverse();
+
+				new_state_k1_k1 = new_state_k1_k + K * (y_k1 - h);
+				new_P_k1_k1 = (Matrix.Identity(K.N) - K * H) * new_P_k1_k;
+			}
+
+			return (new_state_k1_k1, new_P_k1_k1);
 		}
 
 		Matrix CalculateF(Vector state)
@@ -219,11 +186,11 @@ namespace NonlinearFilter
 			return H;
 		}
 
-		Vector GetNextState(Vector state)
+		Vector GetNextState(Vector state, double time)
 		{
 			Vector newstate = new Vector(state.N);
 			for (int i = 0; i < newstate.N; i++)
-				newstate[i] = Info.f[i](state) + Info.u[i](CurrentTime);
+				newstate[i] = Info.f[i](state) + Info.u[i](time);
 
 			return newstate;
 		}
--- NonlinearFilter/Program.cs
+++ NonlinearFilter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Math;
 
 namespace NonlinearFilter
@@ -118,57 +119,40 @@ namespace NonlinearFilter
 			info.h[0] = (Vector v) => v[0];
 			info.h[1] = (Vector v) => v[1];
 
-			info.InitialState = new Vector(5);
-			info.InitialP = new Matrix(5);
-
 			return info;
 		}
 
 		static void FilterTest1()
 		{
-			FilterInfo info = new FilterInfo();
-			info.Q = new Matrix(5);
-			info.R = new Matrix(2);
+			FilterInfo info = CreateModel();
 
-			info.Measurements = new Vector[10];
+			Vector[] Measurements = new Vector[10];
 
 			for (int i = 0; i < 10; i++)
 			{
-				info.Measurements[i] = new Vector(2);
-				info.Measurements[i][0] = (i + 1) * 0.1;
-				info.Measurements[i][1] = 0.0;
+				Measurements[i] = new Vector(2);
+				Measurements[i][0] = (i + 1) * 0.1;
+				Measurements[i][1] = 0.0;
 			}
 
-			info.XCount = 5;
-			info.YCount = 2;
-
-			info.dt = 0.1;
-
-			info.f = new Func<Vector, double>[info.XCount];
-			info.h = new Func<Vector, double>[info.YCount];
-
-			info.f[0] = (Vector v) => v[4] * Cos(v[2]) * info.dt + v[0];
-			info.f[1] = (Vector v) => v[4] * Sin(v[2]) * info.dt + v[1];
-			info.f[2] = (Vector v) => v[3] * info.dt + v[2];
-			info.f[3] = (Vector v) => v[3];
-			info.f[4] = (Vector v) => v[4];
-
-			info.h[0] = (Vector v) => v[0];
-			info.h[1] = (Vector v) => v[1];
-
-			info.InitialP = new Matrix(5);
+			Matrix InitialP = new Matrix(5);
 			for (int i = 0; i < 5; i++)
-				info.InitialP[i, i] = (0.01 * 0.01);
+				InitialP[i, i] = (0.01 * 0.01);
 
-			info.InitialState = new Vector(5);
-			info.InitialState[0] = 0.0;
-			info.InitialState[1] = 0.0;
-			info.InitialState[2] = 0.0;
-			info.InitialState[3] = 0.0;
-			info.InitialState[4] = 1.0;
+			Vector InitialState = new Vector(5);
+			InitialState[0] = 0.0;
+			InitialState[1] = 0.0;
+			InitialState[2] = 0.0;
+			InitialState[3] = 0.0;
+			InitialState[4] = 1.0;
 
 			NonlinearFilter filter = new NonlinearFilter(info);
-			filter.Solve(10);
+
+			Vector state = InitialState.Clone();
+			Matrix P = InitialP.Clone();
+
+			for (int i = 0; i < 5; i++)
+				(state, P) = filter.GetNext(state, P, Measurements[i]);
 		}
 
 		static void FilterTest2()
@@ -177,11 +161,12 @@ namespace NonlinearFilter
 			FilterInfo info = CreateModel();
 
 			// Initial State
-			info.InitialState[0] = 5.0;
-			info.InitialState[1] = 3.0;
-			info.InitialState[2] = 1.0;
-			info.InitialState[3] = 0.0;
-			info.InitialState[4] = 1.0;
+			Vector InitialState = new Vector(5);
+			InitialState[0] = 5.0;
+			InitialState[1] = 3.0;
+			InitialState[2] = 1.0;
+			InitialState[3] = 0.0;
+			InitialState[4] = 1.0;
 
 			info.u = new Func<double, double>[5];
 			info.u[0] = (double t) => 0.0;
@@ -191,9 +176,10 @@ namespace NonlinearFilter
 			info.u[4] = (double t) => 0.0;
 
 			// Initial covariance	
+			Matrix InitialP = new Matrix(5);
 			for (int i = 0; i < 5; i++)
 			{
-				info.InitialP[i, i] = 0.0001;
+				InitialP[i, i] = 0.0001;
 
 				if (i >= 3)
 					info.Q[i, i] = 0.0001;
@@ -203,12 +189,12 @@ namespace NonlinearFilter
 			info.R[1, 1] = (1.0);
 
 			// Generate measurements
-			info.Measurements = new Vector[100];
-			Vector X0 = info.InitialState;
-			info.Measurements[0] = new Vector(2);
-			info.Measurements[0][0] = X0[0];
-			info.Measurements[0][1] = X0[1];
-			Console.WriteLine($"{info.Measurements[0][0]}, {info.Measurements[0][1]}");
+			Vector[] Measurements = new Vector[100];
+			Vector X0 = InitialState;
+			Measurements[0] = new Vector(2);
+			Measurements[0][0] = X0[0];
+			Measurements[0][1] = X0[1];
+			Console.WriteLine($"{Measurements[0][0]}, {Measurements[0][1]}");
 
 			for (int i = 1; i < 100; i++)
 			{
@@ -219,19 +205,24 @@ namespace NonlinearFilter
 				X1[3] = info.f[3](X0);
 				X1[4] = info.f[4](X0);
 
-				info.Measurements[i] = new Vector(2);
-				info.Measurements[i][0] = X1[0];
-				info.Measurements[i][1] = X1[1] * ((-1.0 + 2.0 * random.NextDouble()) * 0.1 + 1);
+				Measurements[i] = new Vector(2);
+				Measurements[i][0] = X1[0];
+				Measurements[i][1] = X1[1] * ((-1.0 + 2.0 * random.NextDouble()) * 0.1 + 1);
 				X0 = X1.Clone();
 
-				Console.WriteLine($"{info.Measurements[i][0]}, {info.Measurements[i][1]}");
+				Console.WriteLine($"{Measurements[i][0]}, {Measurements[i][1]}");
 			}
 
 			NonlinearFilter filter = new NonlinearFilter(info);
-			filter.Solve(70);
 
-			foreach (Vector x in filter.X)
-				Console.WriteLine($"{x[0]}, {x[1]}");
+			Vector state = InitialState.Clone();
+			Matrix P = InitialP.Clone();
+
+			for (int i = 0; i < 5; i++)
+			{
+				(state, P) = filter.GetNext(state, P, Measurements[i]);
+				Console.WriteLine($"{state[0]}, {state[1]}");
+			}
 		}
 
 		static void OneStepFilterTest1()
@@ -240,11 +231,12 @@ namespace NonlinearFilter
 			FilterInfo info = CreateModel();
 
 			// Initial State
-			info.InitialState[0] = 5.0;
-			info.InitialState[1] = 3.0;
-			info.InitialState[2] = 0.0;
-			info.InitialState[3] = 0.0;
-			info.InitialState[4] = 1.0;
+			Vector InitialState = new Vector(5);
+			InitialState[0] = 5.0;
+			InitialState[1] = 3.0;
+			InitialState[2] = 1.0;
+			InitialState[3] = 0.0;
+			InitialState[4] = 1.0;
 
 			info.u = new Func<double, double>[5];
 			info.u[0] = (double t) => 0.0;
@@ -254,24 +246,25 @@ namespace NonlinearFilter
 			info.u[4] = (double t) => 0.0;
 
 			// Initial covariance	
+			Matrix InitialP = new Matrix(5);
 			for (int i = 0; i < 5; i++)
 			{
-				info.InitialP[i, i] = 0.0001;
+				InitialP[i, i] = 0.01 * 0.01;
 
 				if (i >= 3)
-					info.Q[i, i] = 0.0001;
+					info.Q[i, i] = 0.01 * 0.01;
 			}
 
 			info.R[0, 0] = (1.0);
 			info.R[1, 1] = (1.0);
 
 			// Generate measurements
-			info.Measurements = new Vector[100];
-			Vector X0 = info.InitialState;
-			info.Measurements[0] = new Vector(2);
-			info.Measurements[0][0] = X0[0];
-			info.Measurements[0][1] = X0[1];
-			Console.WriteLine($"{info.Measurements[0][0]}, {info.Measurements[0][1]}");
+			Vector[] Measurements = new Vector[100];
+			Vector X0 = InitialState;
+			Measurements[0] = new Vector(2);
+			Measurements[0][0] = X0[0];
+			Measurements[0][1] = X0[1];
+			Console.WriteLine($"{Measurements[0][0]}, {Measurements[0][1]}");
 
 			for (int i = 1; i < 100; i++)
 			{
@@ -282,19 +275,117 @@ namespace NonlinearFilter
 				X1[3] = info.f[3](X0);
 				X1[4] = info.f[4](X0);
 
-				info.Measurements[i] = new Vector(2);
-				info.Measurements[i][0] = X1[0];
-				info.Measurements[i][1] = X1[1] * ((-1.0 + 2.0 * random.NextDouble()) * 0.1 + 1);
+				Measurements[i] = new Vector(2);
+				Measurements[i][0] = X1[0];
+				Measurements[i][1] = X1[1] * ((-1.0 + 2.0 * random.NextDouble()) * 0.1 + 1);
 				X0 = X1.Clone();
 
-				Console.WriteLine($"{info.Measurements[i][0]}, {info.Measurements[i][1]}");
+				Console.WriteLine($"{Measurements[i][0]}, {Measurements[i][1]}");
+			}
+			
+			OneStepIterationFilter filter = new OneStepIterationFilter(info);
+
+			Vector state = InitialState.Clone();
+			Matrix P = InitialP.Clone();
+			Vector Measurement = new Vector(2);
+
+			Console.WriteLine($"{state[0]}, {state[1]}");
+
+			for (int i = 0; i < 99; i++)
+			{
+				(state, P) = filter.GetNext(state, P, Measurements[i + 1]);
+				Console.WriteLine($"{state[0]}, {state[1]}");
+			}
+		}
+
+		static void OneStepFilterTest2()
+		{
+			Random random = new Random();
+			FilterInfo info = CreateModel();
+
+			// Initial State
+			Vector InitialState = new Vector(5);
+			InitialState[0] = 5.0;
+			InitialState[1] = 3.0;
+			InitialState[2] = 1.0;
+			InitialState[3] = 0.0;
+			InitialState[4] = 1.0;
+
+			double w = 0.0;
+			info.u = new Func<double, double>[5];
+			info.u[0] = (double t) => 0.0;
+			info.u[1] = (double t) => 0.0;
+			info.u[2] = (double t) => 0.0;
+			info.u[3] = (double t) =>
+			{
+				if (t < 1.0)
+				{
+					w += 0.1;
+					return 0.1;
+				}
+				else if (w > 0)
+				{
+					w -= 0.1;
+					return -0.1;
+				}
+				else
+				{
+					return 0.0;
+				}
+			};
+			info.u[4] = (double t) => 0.0;
+
+			// Initial covariance	
+			Matrix InitialP = new Matrix(5);
+			for (int i = 0; i < 5; i++)
+			{
+				InitialP[i, i] = 0.01 * 0.01;
+
+				if (i >= 3)
+					info.Q[i, i] = 0.01 * 0.01;
+			}
+
+			info.R[0, 0] = (0.1 * 0.1);
+			info.R[1, 1] = (0.1 * 0.1);
+
+			// Generate measurements
+			Vector[] Measurements = new Vector[100];
+			Vector X0 = InitialState;
+			Measurements[0] = new Vector(2);
+			Measurements[0][0] = X0[0];
+			Measurements[0][1] = X0[1];
+			Console.WriteLine($"{Measurements[0][0]}, {Measurements[0][1]}");
+
+			for (int i = 1; i < 100; i++)
+			{
+				Vector X1 = new Vector(X0.N);
+				X1[0] = info.f[0](X0) + info.u[0](i * info.dt);
+				X1[1] = info.f[1](X0) + info.u[1](i * info.dt);
+				X1[2] = info.f[2](X0) + info.u[2](i * info.dt);
+				X1[3] = info.f[3](X0) + info.u[3](i * info.dt);
+				X1[4] = info.f[4](X0) + info.u[4](i * info.dt);
+
+				Measurements[i] = new Vector(2);
+				Measurements[i][0] = X1[0];
+				Measurements[i][1] = X1[1] * ((-1.0 + 2.0 * random.NextDouble()) * 0.05 + 1);
+				X0 = X1.Clone();
+
+				Console.WriteLine($"{Measurements[i][0]}, {Measurements[i][1]}");
 			}
 
 			OneStepIterationFilter filter = new OneStepIterationFilter(info);
-			filter.Solve(70);
 
-			foreach (Vector x in filter.X)
-				Console.WriteLine($"{x[0]}, {x[1]}");
+			Vector state = InitialState.Clone();
+			Matrix P = InitialP.Clone();
+			Vector Measurement = new Vector(2);
+
+			Console.WriteLine($"{state[0]}, {state[1]}");
+
+			for (int i = 0; i < 99; i++)
+			{
+				(state, P) = filter.GetNext(state, P, Measurements[i + 1]);
+				Console.WriteLine($"{state[0]}, {state[1]}");
+			}
 		}
 
 		static void Main(string[] args)
@@ -303,8 +394,7 @@ namespace NonlinearFilter
 			customCulture.NumberFormat.NumberDecimalSeparator = ".";
 			System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
 
-			OneStepFilterTest1();
-			//FilterTest2();
+			OneStepFilterTest2();
 		}
 	}
 }
